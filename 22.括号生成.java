07fec3e25a9c0369bb6038067aa5b74abf2c3966
Jiang Nan <jnriver1986@gmail.com>
2021-09-26 14:30:57 +0800

    每日一题

--- 22.括号生成.java
+++ 22.括号生成.java
@@ -14,39 +14,40 @@
  * Testcase Example:  '3'
  *
  * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
- * 
+ *
  * 有效括号组合需满足：左括号必须以正确的顺序闭合。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：n = 3
  * 输出：["((()))","(()())","(())()","()(())","()()()"]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：n = 1
  * 输出：["()"]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 1 <= n <= 8
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
 class Solution {
     public List<String> generateParenthesis(int n) {
-
+        List<String> res = new ArrayList<String>();
+        return res;
     }
 }
 // @lc code=end
