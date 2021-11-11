28296a30b1d77e6f163d8ec2e0c6d9baaad6f071
Jiang Nan <jnriver1986@gmail.com>
2021-11-11 09:36:20 +0800

    :arrow_up: Daily programs

---
+++ 367.有效的完全平方数.java
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=367 lang=java
+ *
+ * [367] 有效的完全平方数
+ *
+ * https://leetcode-cn.com/problems/valid-perfect-square/description/
+ *
+ * algorithms
+ * Easy (43.86%)
+ * Likes:    291
+ * Dislikes: 0
+ * Total Accepted:    105.3K
+ * Total Submissions: 235.4K
+ * Testcase Example:  '16'
+ *
+ * 给定一个 正整数 num ，编写一个函数，如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
+ *
+ * 进阶：不要 使用任何内置的库函数，如  sqrt 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：num = 16
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：num = 14
+ * 输出：false
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long x = 1, square = 1;
+        while (square <= num) {
+            if (square == num) {
+                return true;
+            }
+            ++x;
+            square = x * x;
+        }
+        return false;
+    }
+}
+// @lc code=end
+
